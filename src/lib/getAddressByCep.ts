8ee40576c866a3a3997e00694809b30fb12e72f9
Mateus Araujo <mateus_1542@hotmail.com>
2021-03-02 12:51:31 -0300

    fix(cep-address): improve getAddressByCEP error handling

--- src/lib/getAddressByCep.ts
+++ src/lib/getAddressByCep.ts
@@ -4,18 +4,30 @@
  * @param {string} cep - CEP: Por exemplo: '60160-110' ou '60160110'
  */
 async function getAddressByCEP(cep: string) {
+    const cepRawValue = cep?.toString().replace(/\D+/g, '')
+
+    if (cepRawValue?.length !== 8) {
+        throw new Error('CEP deve conter exatamente 8 números.')
+    }
+
     try {
         const response = await fetch(
-            `https://viacep.com.br/ws/${cep}/json`
+            `https://viacep.com.br/ws/${cepRawValue}/json`
         )
 
         const data = await response.json()
 
+        if (data?.erro === true) {
+            throw data
+        }
+
         return data
     } catch (error) {
-        return {
-            message: 'Erro na requisição ao ViaCEP'
+        if (error?.erro === true) {
+            throw new Error('CEP não encontrado')
         }
+
+        throw new Error('Erro na requisição ao ViaCEP')
     }
 }
 
